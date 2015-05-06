628fd5a36b510581b1ddec569ad7f4f979711b04
Yumiko Huang <dayumikoda@gmail.com>
2015-05-06 10:34:22 -0700

    cleaed code, should be able to update database now

--- app/controllers/ereports_controller.rb
+++ app/controllers/ereports_controller.rb
@@ -1,29 +1,21 @@
 class EreportsController < ApplicationController
+	include EreportsHelper
 	def create
 		require 'open-uri'
-		search_result = Stock.where(symbol:params[:symbol])
+		stock_symbol = params[:symbol]
+		search_result = Stock.where(symbol:stock_symbol)
 
 		if search_result.empty?
-			@stock = Stock.new(symbol:params[:symbol])
+			@stock = Stock.new(symbol:stock_symbol)
 			@stock.save
+			stock_id = @stock.id
+			
 			p "stock stored in db, where stock_id = #{@stock.id}"
 			qrt = [1,2,3,4]
-			year = [2012, 2013, 2014, 2015]
+			year = (2012..Date.today.year).to_a
 			year.each do |each_year|
 				qrt.each do |each_qrt|
-					url = "http://www.estimize.com/#{params[:symbol]}/fq#{each_qrt}-#{each_year}#chart=historical"
-					begin 
-						page = Nokogiri::HTML(open(url))
-					rescue
-						p "no reports for #{each_year} #{each_qrt}"
-					else
-						page.search('.date').map do |element|
-							temp = (element.inner_text[0..-5]).split('/')
-							date_str = (temp[1] +'/'+ temp[0] +'/20'+ temp[2]).to_s
-							earning_report = Ereport.create(symbol:params[:symbol], date:date_str, stock_id:@stock.id)
-							p earning_report.stock_id
-						end	
-					end
+					EreportsHelper.get_earning_report_dates(stock_symbol, stock_id, each_qrt, each_year)
 				end
 			end
 			@all_ers = search_result[0].ereports
@@ -35,91 +27,24 @@ class EreportsController < ApplicationController
 				p "do nothing"
 			end
 			@html = []
+
 			@all_ers.each do |earning|
-				the_day = earning.date
-				if the_day <= Date.today
-					formatted_date = the_day.strftime("%m/%d/%Y").to_s.split("/")
-					month = formatted_date[0].to_i
-					date = formatted_date[1].to_i
-					year = formatted_date[2].to_i
-					if (the_day.month == the_day.next_day.month) && (!the_day.next_day.saturday?) && (the_day.month == the_day.prev_day.month)
-						url = "http://finance.yahoo.com/q/hp?s=#{@stock}&a=#{month-1}&b=#{date-1}&c=#{year}&d=#{month-1}&e=#{date+1}&f=#{year}&g=d"
-					elsif (the_day.month != the_day.prev_day.month) && (!the_day.next_day.saturday?)
-						special_date = the_day.prev_month.end_of_month.strftime('%Y/%m/%d').to_s.split('/')
-						url = "http://finance.yahoo.com/q/hp?s=#{@stock}&a=#{month-2}&b=#{special_date[2].to_i}&c=#{year}&d=#{month-1}&e=#{date+1}&f=#{year}&g=d"
-					elsif (the_day.month != the_day.prev_day.month) && (the_day.next_day.saturday?)
-						special_date = the_day.prev_month.end_of_month.strftime('%Y/%m/%d').to_s.split('/')
-						url = "http://finance.yahoo.com/q/hp?s=#{@stock}&a=#{month-2}&b=#{special_date[2].to_i}&c=#{year}&d=#{month-1}&e=#{date+3}&f=#{year}&g=d"						
-					elsif (the_day.month != the_day.next_day.month) && (!the_day.next_day.saturday?) && (the_day.month == the_day.prev_day.month)
-						url = "http://finance.yahoo.com/q/hp?s=#{@stock}&a=#{month-1}&b=#{date-1}&c=#{year}&d=#{month}&e=1&f=#{year}&g=d"
-					elsif (the_day.month != the_day.next_day.month) && (the_day.next_day.saturday?) && (the_day.month == the_day.prev_day.month)
-						url = "http://finance.yahoo.com/q/hp?s=#{@stock}&a=#{month-1}&b=#{date-1}&c=#{year}&d=#{month}&e=3&f=#{year}&g=d"
-					elsif (the_day.month == the_day.next_day.month) && (the_day.next_day.saturday?) && (the_day.month == the_day.prev_day.month)
-						url = "http://finance.yahoo.com/q/hp?s=#{@stock}&a=#{month-1}&b=#{date-1}&c=#{year}&d=#{month-1}&e=#{date+3}&f=#{year}&g=d"
-					end
-					p "#{the_day} #{url}"
-					page = Nokogiri::HTML(open(url))
-					page.search('.yfnc_datamodoutline1').map do |element|
-						# @html.push(element.inner_html)
-						pricing_array = []
-						price_before_er = PriceBeforeEr.create(ereport_id:earning.id)
-						price_on_er = PriceOnEr.create(ereport_id:earning.id)
-						price_after_er = PriceAfterEr.create(ereport_id:earning.id)
-						array_of_quotes = element.css('.yfnc_tabledata1')
-						array_of_quotes.each do |quote|
-							p html_str = quote.inner_text
-							if  html_str.match('[a-zA-Z]{3}\s\d{1,2}\,\s\d{4}')
-								er_date = Date.parse(html_str)
-								if er_date < the_day
-									price_before_er.price_date = er_date
-									price_before_er.save
-								elsif er_date > the_day
-									price_after_er.price_date = er_date
-									price_after_er.save
-								else
-									price_on_er.price_date = er_date
-									price_on_er.save
-								end
-							elsif html_str.match('\d*\.\d*')
-								price = html_str.to_f
-								if price_before_er.price_date && (price_before_er.quote.length != 5)
-									price_before_er.quote.push(price)
-									price_before_er.save
-								elsif price_after_er.price_date && (price_after_er.quote.length != 5)
-									price_after_er.quote.push(price)
-									price_after_er.save
-								elsif ( price_on_er.quote.length != 5)
-									price_on_er.quote.push(price)
-									price_on_er.save
-								end
-
-							else
-								stock_volume = html_str.tr(",","").to_i
-								p "this volume"
-								if price_before_er.price_date
-									price_before_er.volume = stock_volume
-									price_before_er.save
-								elsif price_on_er.price_date
-									price_on_er.volume = stock_volume
-									price_on_er.save
-								elsif price_after_er.price_date
-									price_after_er.volume = stock_volume
-									price_after_er.save
-								end
-							end
-
-						end
-					end
-				else
-					p the_day
-				end
+				EreportsHelper.earning_report_dates_data(stock_symbol, earning)
 			end
-			this_stock = Stock.where(symbol:params[:symbol])[0]
+
+			this_stock = Stock.where(symbol:stock_symbol)[0]
 			@all_reports = this_stock.ereports
 			render 'stocks/show'
 		else
 			@stock = params[:symbol]
+			stock_symbol = @stock
 			@stock_info = Stock.where(symbol:params[:symbol])[0]
+			all_reports = @stock_info.ereports
+			all_reports.each do |report|
+				if (report.date <= Date.today && !report.price_before_er)
+					EreportsHelper.earning_report_dates_data(stock_symbol, report)
+				end
+			end
 			@all_reports = @stock_info.ereports
 			@no_report = false
 			if @all_reports.count == 0
