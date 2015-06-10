aaccc7794c8dc8ec8402fb3bb33f4b669cb8f534
Yumiko Huang <dayumikoda@gmail.com>
2015-06-10 00:14:41 -0700

    changed env to production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # every :day, :at => '3:25 pm' do
 
-set :environment, "development"
+set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every :day, :at => "7am" do
