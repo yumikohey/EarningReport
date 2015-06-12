3e0311597008340031d14c5a6abd9eab8abe0ea8
Yumiko Huang <dayumikoda@gmail.com>
2015-06-11 20:57:19 -0700

    run all stocks' options chain on production database

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,6 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every :day, :at => "7am" do
-  rake 'daily_option_chains'
-end
-
-every :day, :at => "12:50am" do
+every :day, :at => "4:15am" do
   rake 'daily_option_chains'
 end
