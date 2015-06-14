404b77b322d2dc24443bb2c602ea008f245f6fdb
Yumiko Huang <dayumikoda@gmail.com>
2015-06-14 06:19:18 -0700

    log in pages and testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -32,8 +34,10 @@ Rails.application.routes.draw do
   get '/current_pain', to: 'stocks#current_pain', as: 'current_pain'
 
 
-  get 'sign_up'  => 'users#new'
-
+  get    'sign_up'   => 'users#new'
+  get    'login'     => 'sessions#new'
+  post   'login'     => 'sessions#create'
+  delete 'logout'    => 'sessions#destroy'
   resources :users
 
   get "dashboards/dashboard_1"
