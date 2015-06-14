15cb162be99a4df712cfcabccffefa11d82145a8
Yumiko Huang <dayumikoda@gmail.com>
2015-06-14 12:32:45 -0700

    subtle bugs fixed for login / logout sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
+
 	before_save { self.email = email.downcase }
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
@@ -13,4 +15,19 @@ class User < ActiveRecord::Base
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
 	end
+
+	def User.new_token
+	  SecureRandom.urlsafe_base64
+	end
+
+	def remember
+	  self.remember_token = User.new_token
+	  update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
 end
