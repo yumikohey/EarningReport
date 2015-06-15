a9ff17f7a1fec54adfcbbb795747166c3cf29d96
Yumiko Huang <dayumikoda@gmail.com>
2015-06-15 16:01:19 -0700

    ignored

--- app/assets/javascripts/historical_prices.js
+++ app/assets/javascripts/historical_prices.js
@@ -1,1 +1,18 @@
 
+$(function(){
+	$('.ajax_next_btn').on('click', function(){
+		$.ajax({
+		           type: "GET",
+		           contentType: "application/json; charset=utf-8",
+		           url: '/next',
+		           dataType: 'json',
+		           success: function (data) {
+		              console.log(data);
+		              $('.aaa_box').append("hello");
+		           },
+		           error: function (result) {
+		               error();
+		           }
+		});
+	});
+});
--- app/controllers/historical_prices_controller.rb
+++ app/controllers/historical_prices_controller.rb
@@ -8,10 +8,7 @@ class HistoricalPricesController < ApplicationController
 	def next
 		@stock = Stock.find(7160)
   	@quote = HistoryQuote.where(stock_id:@stock.id).first
-  	respond_to do |format|
-  		#format.html { redirect_to history_index_path }
-  		format.js
-  	end
+  	render "index"
 	end	
 
 end
