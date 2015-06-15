db1057049ddb49fe0f877323189c3ebd29a1ed8f
Yumiko Huang <dayumikoda@gmail.com>
2015-06-14 23:52:10 -0700

    schedule historical data

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,3 +4,7 @@ set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log
 every :day, :at => "11:00pm" do
   rake 'daily_option_chains'
 end
+
+every '5 6 15 * *' do
+	rake 'history_quotes'
+end
