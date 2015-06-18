33300afe803f86654e77d5f788bae203bdfd42a1
Ubuntu <ubuntu@ip-172-31-14-73.us-west-1.compute.internal>
2015-06-18 00:59:27 +0000

    slight changes from server

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every :weekday => "11:00pm" do
+every :weekday => "9:00pm" do
   rake 'daily_option_chains'
 end
 
