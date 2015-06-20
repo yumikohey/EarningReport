b076a0701826bf19762e92db7a4a8140ab6e8cc3
Yumiko Huang <dayumikoda@gmail.com>
2015-06-20 00:51:03 -0700

    fixed bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+  
   def new
     @admin_page = false
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
   def new
   	@user = User.new
     @admin_page = false
