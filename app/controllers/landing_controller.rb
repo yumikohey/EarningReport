2956b0b3cab35c099739213e4fe52b9e4f57248a
Yumiko Huang <dayumikoda@gmail.com>
2015-06-21 02:15:40 -0700

    there still a subtle bug

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,6 @@
 class LandingController < ApplicationController
   def index
-  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: Date.parse('2015-06-19')).paginate(:page => params[:page], :per_page => 20)
-  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: Date.parse('2015-06-19')).paginate(:page => params[:page], :per_page => 20)
+  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: Date.parse('2015-06-19')).paginate(:page => params[:golden_page], :per_page => 10)
+  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: Date.parse('2015-06-19')).paginate(:page => params[:death_page], :per_page => 10)
   end
 end
