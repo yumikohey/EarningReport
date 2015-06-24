6a4e0e491b91a756e627e47a3729602d4bbcf0d0
Yumiko Huang <dayumikoda@gmail.com>
2015-06-23 20:40:56 -0700

    done

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,6 @@
 class LandingController < ApplicationController
   def index
-  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: Date.parse('2015-06-22')).first(10)
-  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: Date.parse('2015-06-22')).first(10)
+  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: Date.parse('2015-06-23')).first(10)
+  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: Date.parse('2015-06-23')).first(10)
   end
 end
