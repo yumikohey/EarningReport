7010217b8507c46253118e7ac35cd10ae64c66cb
Yumiko Huang <dayumikoda@gmail.com>
2015-06-27 18:16:10 -0700

    some golden cross layout modifications

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,14 +1,14 @@
 class LandingController < ApplicationController
   def index
-  	today = BetaQuote.last.date
-  	if BetaQuote.where(date:today).where(cross:1).count > 0
-	  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: today).first(10)
-	  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: today).first(10)
+  	@today = BetaQuote.last.date
+  	if BetaQuote.where(date:@today).where(cross:1).count > 0
+	  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: @today).first(10)
+	  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: @today).first(10)
 	  else
-	  	while (BetaQuote.where(date:today).where(cross:1).count <= 0) do
-	  		today -= 1
-		  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: today).first(10)
-		  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: today).first(10)
+	  	while (BetaQuote.where(date:@today).where(cross:1).count <= 0) do
+	  		@today -= 1
+		  	@golden_cross_stocks = BetaQuote.where(cross:1).where(date: @today).first(10)
+		  	@death_cross_stocks = BetaQuote.where(cross:-1).where(date: @today).first(10)
 		  end
 	  end
   end
