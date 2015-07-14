a2c80bda7fa023e1bbb00290dfbb02640822130a
Yumiko Huang <dayumikoda@gmail.com>
2015-07-14 02:57:49 -0700

    options

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every %w( mon tue wed thu fri ), :at => "10:30pm" do
+every %w( mon tue wed thu fri ), :at => "10:00am" do
   rake 'daily_option_chains'
 end
 
@@ -21,13 +21,13 @@ end
 # 	rake 'golden_cross'
 # end
 
-every :tuesday, :at => "8:52am" do
-	rake 'five_avg_daily'
-end
+# every :tuesday, :at => "8:52am" do
+# 	rake 'five_avg_daily'
+# end
 
-every :tuesday, :at => "10:00am" do
-	rake 'golden_cross'
-end
+# every :tuesday, :at => "10:00am" do
+# 	rake 'golden_cross'
+# end
 
 # every :monday => "1:00am" do
 # 	rake 'earning_report'
