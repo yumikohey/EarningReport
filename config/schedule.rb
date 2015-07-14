912e5387836dcb7db369b1885f6ab84705ace9c1
Yumiko Huang <dayumikoda@gmail.com>
2015-07-14 14:34:27 -0700

    new schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,34 +1,24 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-# every %w( tue wed thu fri ), :at => "10:00pm" do
-#   rake 'daily_option_chains'
-# end
+every %w( mon tue wed thu ), :at => "9:45pm" do
+  rake 'daily_option_chains'
+end
 
 
-# every %w( mon tue wed thu fri), :at => "09:30pm" do
-# 	rake 'daily_quote'
-# 	rake 'beta_daily_quote'
-# end
+every %w( mon tue wed thu fri), :at => "9:40pm" do
+	rake 'daily_quote'
+	rake 'beta_daily_quote'
+end
 
 
-# every %w( mon tue wed thu fri), :at => "10:30pm" do
-# 	rake 'five_avg_daily'
-# end
-
-
-# every %w( tue wed thu fri sat), :at => "03:00am" do
-# 	rake 'golden_cross'
-# end
-
-every :sunday, :at => "7:00am" do
+every %w( mon tue wed thu fri), :at => "10:30pm" do
 	rake 'five_avg_daily'
 end
 
-# every :monday => "1:00am" do
-# 	rake 'earning_report'
-# end
 
-# every :saturday => "2:00am" do
-# 	rake 'update_db'
-# end
+every %w( mon tue wed thu fri), :at => "11:30pm" do
+	rake 'golden_cross'
+end
+
+
